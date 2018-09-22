40861b0b7d6fa92bee8c66b477ae6738a362b18c
Kevin Stähler <phrenchen@gmail.com>
2018-09-22 14:11:50 +0200

    initial commit. webpack compiles typescript files onChange

---
+++ src/Main.ts
@@ -0,0 +1,10 @@
+export class Main{
+    constructor(){
+        console.log("constructing main");
+        this.init();
+    }
+
+    private init():void{
+        console.log("init main");
+    }
+}
